4b7377dfccf939288700525bf1c64bf424d22167
alfi67-git <xafal3x@gmail.com>
2025-05-31 01:20:49 +0200

    modification lien d'edit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/alfi67-git/alfi-website/',
+            'https://github.com/alfi67-git/alfi-website/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
@@ -53,7 +53,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/alfi67-git/alfi-website/',
+            'https://github.com/alfi67-git/alfi-website/tree/main/blog',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
